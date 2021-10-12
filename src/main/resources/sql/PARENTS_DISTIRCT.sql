102e11d719c9c798e28edde16f5fc81d934cdec2
김재성 <aamoos@naver.com>
2021-10-12 15:59:31 +0900

    sql문 백업

---
+++ src/main/resources/sql/PARENTS_DISTIRCT.sql
@@ -0,0 +1,23 @@
+/*
+-- Query: SELECT * FROM travel.
+LIMIT 0, 1000
+
+-- Date: 2021-10-12 15:21
+*/
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (1,'서울','Y');
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (2,'부산','Y');
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (3,'대구','Y');
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (4,'인천','Y');
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (5,'광주','Y');
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (6,'대전','Y');
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (7,'울산','Y');
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (8,'세종','Y');
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (9,'경기','Y');
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (10,'강원','Y');
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (11,'충븍','Y');
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (12,'충남','Y');
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (13,'전북','Y');
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (14,'전남','Y');
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (15,'경북','Y');
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (16,'경남','Y');
+INSERT INTO parents_district (`parents_idx`,`district_name`,`use_yn`) VALUES (17,'제주','Y');
